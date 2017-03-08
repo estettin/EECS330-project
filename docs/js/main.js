56789a4b325ecab488a4ab700d2014b3e850b037
Elana Stettin <Elana@dhcp-10-105-176-173.wireless.northwestern.private>
2017-03-07 21:59:04 -0600

    dropdown

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-     $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
 
-     var $bars = $();
-     $('.progress-bar').each( function() {
-     	if ($(this).width() == 0)
-     		$(this).hide();
-     
-     });
+    var $bars = $();
+    $('.progress-bar').each(function() {
+        if ($(this).width() == 0)
+            $(this).hide();
 
-     $('.collapsible').collapsible();
-     
+    });
+
+    $('.collapsible-header').on('click', function() {
+    	$(this).children().children('.fa').toggleClass('fa-angle-down fa-angle-up');
+    });
+
+    $('.collapsible').collapsible();
+
 });
